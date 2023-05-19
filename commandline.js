97a0bc2b0c146dc2016f97c7f7bc58e528189c32
Asish Jena <ashpassionpro@gmail.com>
2023-05-20 01:57:48 +0530

    Update commandline.js

--- commandline.js
+++ commandline.js
@@ -1,1 +1,9 @@
-console.log(process.argv[3]);
+// console.log(process.argv[3]);
+
+var a = parseInt(process.argv[2]);
+var o = process.argv[3];
+var b = parseInt(process.argv[4]);
+
+if (o == "+") {
+  console.log(a + b);
+}
