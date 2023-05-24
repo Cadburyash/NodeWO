07fb3ad256a06444af9784d47b5467be8c786b4e
Asish Jena <ashpassionpro@gmail.com>
2023-05-25 01:54:04 +0530

    updatedata file

--- data/updatedata.js
+++ data/updatedata.js
@@ -5,3 +5,11 @@ var movieData = JSON.parse(fs.readFileSync("movies.json").toString());
 movieData.Year = 2002;
 
 console.log(movieData);
+
+fs.writeFile("movies.json", JSON.stringify(movieData), (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Files updated");
+  }
+});
