21a62347262a21555bc08dc7da56d7642604a6f7
Asish Jena <ashpassionpro@gmail.com>
2023-05-25 01:35:40 +0530

    json files
    movies

--- data/data.js
+++ data/data.js
@@ -1,1 +1,16 @@
- 
+const dataObj = {
+  movieName: "Creeper jeepers",
+  Year: 2001,
+};
+
+console.log(JSON.stringify(dataObj));
+
+const fs = require("fs");
+
+fs.writeFile("movies.json", JSON.stringify(dataObj), (err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("file written");
+  }
+});
