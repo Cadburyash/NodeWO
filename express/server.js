6963d9f8c1a37d1dd85bd26714f213ec7e8cfd83
Asish Jena <ashpassionpro@gmail.com>
2023-05-27 02:45:38 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -7,15 +7,15 @@ app.get("/", (req, res) => {
   res.send("welcome to Express server");
 });
 
-app.get("/getMovies", (req, res) => {
-  fs.readFile("./data/db.json", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(JSON.parse(data));
-    }
-  });
-});
+// app.get("/getMovies", (req, res) => {
+//   fs.readFile("./data/db.json", (err, data) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(JSON.parse(data));
+//     }
+//   });
+// });
 
 app.listen(port, (err) => {
   if (err) {
