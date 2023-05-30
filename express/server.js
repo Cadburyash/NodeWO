c295aa80791917cc97bf97317c235886b2167fc1
Asish Jena <ashpassionpro@gmail.com>
2023-05-31 02:21:57 +0530

    express static

--- express/server.js
+++ express/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 8080;
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   res.send("welcome to Express server");
 });
